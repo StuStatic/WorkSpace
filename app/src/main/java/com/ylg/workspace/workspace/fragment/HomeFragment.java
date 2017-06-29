a45b6f45c3bf67c1058ad97727fb223a15bd9f37
duanyangyang <m13240371551@163.com>
2017-06-29 13:44:39 +0800

    段洋洋  我的界面的搭建与3D效果的修改

--- app/src/main/java/com/ylg/workspace/workspace/fragment/HomeFragment.java
+++ app/src/main/java/com/ylg/workspace/workspace/fragment/HomeFragment.java
@@ -27,21 +27,21 @@ public class HomeFragment extends android.app.Fragment {
     private View homeLayout;
     private ViewPager mViewPager;
     private List<ImageView> mImageViewList;
-    private int[] images={R.mipmap.a1,R.mipmap.a2,R.mipmap.a3,R.mipmap.a4,R.mipmap.a5};
-    private int currentPosition=1;
-    private int dotPosition=0;
-    private int prePosition=0;
+    private int[] images = {R.mipmap.a1, R.mipmap.a2, R.mipmap.a3, R.mipmap.a4, R.mipmap.a5};
+    private int currentPosition = 1;
+    private int dotPosition = 0;
+    private int prePosition = 0;
     private LinearLayout mLinearLayoutDot;
     private List<ImageView> mImageViewDotList;
     private FrameLayout frameLayout;
     private TagCloudView tcv;
     private List<String> mStrings;
-    Handler handler=new Handler(){
+    Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            if(msg.what==1){
-                mViewPager.setCurrentItem(currentPosition,false);
+            if (msg.what == 1) {
+                mViewPager.setCurrentItem(currentPosition, false);
             }
         }
     };
@@ -56,8 +56,8 @@ public class HomeFragment extends android.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         homeLayout = inflater.inflate(R.layout.fragment_home, container, false);
-        mViewPager= (ViewPager)homeLayout.findViewById(R.id.vp_main);
-        mLinearLayoutDot= (LinearLayout)homeLayout.findViewById(R.id.ll_main_dot);
+        mViewPager = (ViewPager) homeLayout.findViewById(R.id.vp_main);
+        mLinearLayoutDot = (LinearLayout) homeLayout.findViewById(R.id.ll_main_dot);
         initView();
 
         initData(homeLayout.getContext());
@@ -70,41 +70,50 @@ public class HomeFragment extends android.app.Fragment {
 
         return homeLayout;
     }
+
     private void initView() {
         mStrings = new ArrayList<>();
-        mStrings.add("张雨不要脸");
-        mStrings.add("张雨是山炮");
-        mStrings.add("赵亮亮不要脸");
-        mStrings.add("赵亮亮是山炮");
-        mStrings.add("赵亮亮没有买票");
-        mStrings.add("张雨没有买票");
-        mStrings.add("哈哈哈");
-        mStrings.add("非常好");
-        mStrings.add("我赞成");
-        mStrings.add("我们都赞成");
+        mStrings.add("活动");
+        mStrings.add("咨询");
+        mStrings.add("友邻");
+        mStrings.add("集市");
+        mStrings.add("定工位");
+        mStrings.add("开门");
+        mStrings.add("发集市");
+        mStrings.add("集市");
+        mStrings.add("访客预约");
+        mStrings.add("预定场地");
+        mStrings.add("精选");
+        mStrings.add("灯光");
+        mStrings.add("集市");
+        mStrings.add("定会议室");
+        mStrings.add("一键保修");
+        mStrings.add("企业介绍");
+        mStrings.add("集市");
+        mStrings.add("集市");
+        mStrings.add("意见反馈");
         tcv = (TagCloudView) homeLayout.findViewById(R.id.tcv);
         TagAdapter tagsAdapter = new TagAdapter(mStrings);
         tcv.setAdapter(tagsAdapter);
-
     }
 
 
     private void initData(Context context) {
-        mImageViewList=new ArrayList<>();
-        mImageViewDotList=new ArrayList();
+        mImageViewList = new ArrayList<>();
+        mImageViewDotList = new ArrayList();
         ImageView imageView;
-        for(int i=0;i<images.length+2;i++){
-            if(i==0){   //判断当i=0为该处的ImageView设置最后一张图片作为背景
-                imageView=new ImageView(context);
-                imageView.setBackgroundResource(images[images.length-1]);
+        for (int i = 0; i < images.length + 2; i++) {
+            if (i == 0) {   //判断当i=0为该处的ImageView设置最后一张图片作为背景
+                imageView = new ImageView(context);
+                imageView.setBackgroundResource(images[images.length - 1]);
                 mImageViewList.add(imageView);
-            }else if(i==images.length+1){   //判断当i=images.length+1时为该处的ImageView设置第一张图片作为背景
-                imageView=new ImageView(context);
+            } else if (i == images.length + 1) {   //判断当i=images.length+1时为该处的ImageView设置第一张图片作为背景
+                imageView = new ImageView(context);
                 imageView.setBackgroundResource(images[0]);
                 mImageViewList.add(imageView);
-            }else{  //其他情况则为ImageView设置images[i-1]的图片作为背景
-                imageView=new ImageView(context);
-                imageView.setBackgroundResource(images[i-1]);
+            } else {  //其他情况则为ImageView设置images[i-1]的图片作为背景
+                imageView = new ImageView(context);
+                imageView.setBackgroundResource(images[i - 1]);
                 mImageViewList.add(imageView);
             }
         }
@@ -113,11 +122,11 @@ public class HomeFragment extends android.app.Fragment {
     //  设置轮播小圆点//
     private void setDot(Context context) {
         //  设置LinearLayout的子控件的宽高，这里单位是像素。
-        LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(15,15);
-        params.rightMargin=20;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(15, 15);
+        params.rightMargin = 20;
         //  for循环创建images.length个ImageView（小圆点）
-        for(int i=0;i<images.length;i++){
-            ImageView  imageViewDot=new ImageView(context);
+        for (int i = 0; i < images.length; i++) {
+            ImageView imageViewDot = new ImageView(context);
             imageViewDot.setLayoutParams(params);
             //  设置小圆点的背景为暗红图片
             imageViewDot.setBackgroundResource(R.mipmap.red_dot_night);
@@ -129,7 +138,7 @@ public class HomeFragment extends android.app.Fragment {
     }
 
     private void setViewPager() {
-        ViewPagerAdater_Home01 adapter=new ViewPagerAdater_Home01(mImageViewList);
+        ViewPagerAdater_Home01 adapter = new ViewPagerAdater_Home01(mImageViewList);
 
         mViewPager.setAdapter(adapter);
 
@@ -143,39 +152,40 @@ public class HomeFragment extends android.app.Fragment {
 
             @Override
             public void onPageSelected(int position) {
-                if(position==0){    //判断当切换到第0个页面时把currentPosition设置为images.length,即倒数第二个位置，小圆点位置为length-1
-                    currentPosition=images.length;
-                    dotPosition=images.length-1;
-                }else if(position==images.length+1){    //当切换到最后一个页面时currentPosition设置为第一个位置，小圆点位置为0
-                    currentPosition=1;
-                    dotPosition=0;
-                }else{
-                    currentPosition=position;
-                    dotPosition=position-1;
+                if (position == 0) {    //判断当切换到第0个页面时把currentPosition设置为images.length,即倒数第二个位置，小圆点位置为length-1
+                    currentPosition = images.length;
+                    dotPosition = images.length - 1;
+                } else if (position == images.length + 1) {    //当切换到最后一个页面时currentPosition设置为第一个位置，小圆点位置为0
+                    currentPosition = 1;
+                    dotPosition = 0;
+                } else {
+                    currentPosition = position;
+                    dotPosition = position - 1;
                 }
                 //  把之前的小圆点设置背景为暗红，当前小圆点设置为红色
                 mImageViewDotList.get(prePosition).setBackgroundResource(R.mipmap.red_dot_night);
                 mImageViewDotList.get(dotPosition).setBackgroundResource(R.mipmap.red_dot);
-                prePosition=dotPosition;
+                prePosition = dotPosition;
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
                 //当state为SCROLL_STATE_IDLE即没有滑动的状态时切换页面
-                if(state==ViewPager.SCROLL_STATE_IDLE){
-                    mViewPager.setCurrentItem(currentPosition,false);
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    mViewPager.setCurrentItem(currentPosition, false);
                 }
             }
         });
     }
+
     //  设置自动播放
     private void autoPlay() {
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 super.run();
 
-                while(true){
+                while (true) {
                     SystemClock.sleep(5000);
                     currentPosition++;
                     handler.sendEmptyMessage(1);
@@ -183,8 +193,6 @@ public class HomeFragment extends android.app.Fragment {
             }
         }.start();
     }
-
-
 
 
 }
