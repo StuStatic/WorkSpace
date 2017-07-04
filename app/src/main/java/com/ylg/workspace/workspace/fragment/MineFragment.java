ba479798f2612835f1621bb4ac51ee726b46a737
duanyangyang <m13240371551@163.com>
2017-07-04 17:51:10 +0800

    段洋洋  我的活动的界面的编写

--- app/src/main/java/com/ylg/workspace/workspace/fragment/MineFragment.java
+++ app/src/main/java/com/ylg/workspace/workspace/fragment/MineFragment.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.ylg.workspace.workspace.R;
+import com.ylg.workspace.workspace.activity.personaldetails.MyActivitys;
 import com.ylg.workspace.workspace.activity.personaldetails.MyBusinessActivity;
 import com.ylg.workspace.workspace.activity.personaldetails.MyInformationActivity;
 import com.ylg.workspace.workspace.activity.personaldetails.SetUpActivity;
@@ -66,7 +67,8 @@ public class MineFragment extends android.app.Fragment implements View.OnClickLi
                 break;
             case R.id.mine_Order_Myactivitys:
                 Log.i("dyy","我的活动");
-
+                Intent myActivitysIntent = new Intent(getActivity(),MyActivitys.class);
+                startActivity(myActivitysIntent);
                 break;
             case R.id.mine_user_Headportrait:
                 Log.i("dyy","我的信息");
