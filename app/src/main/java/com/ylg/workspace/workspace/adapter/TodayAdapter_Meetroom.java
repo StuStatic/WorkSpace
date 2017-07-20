08de9e442ac318efb4773017b6cd762fbf604498
stu <14722828229@163.com>
2017-07-20 23:36:12 +0800

    stu-定会议室界面

--- app/src/main/java/com/ylg/workspace/workspace/adapter/TodayAdapter_Meetroom.java
+++ app/src/main/java/com/ylg/workspace/workspace/adapter/TodayAdapter_Meetroom.java
@@ -6,7 +6,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ylg.workspace.workspace.R;
 
@@ -19,9 +21,13 @@ import java.util.List;
 public class TodayAdapter_Meetroom extends BaseAdapter {
     private List<String> datas;
     private LayoutInflater inflater;
+    private Context context;
+    private RelativeLayout item1,item2;
+    private int currentItem = -1; //用于记录点击的 Item 的 position，是控制 item 展开的核心
     //构造方法
     public TodayAdapter_Meetroom(Context context , List<String> datas){
         this.datas = datas;
+        this.context = context;
         inflater = LayoutInflater.from(context);
     }
     @Override
@@ -35,12 +41,12 @@ public class TodayAdapter_Meetroom extends BaseAdapter {
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int position, View view, ViewGroup viewGroup) {
         TodayAdapter_Meetroom.ViewHolder viewHolder;
         if(view==null){
             view=inflater.inflate(R.layout.item_meetroomtodaylv,viewGroup ,false);
@@ -50,19 +56,49 @@ public class TodayAdapter_Meetroom extends BaseAdapter {
             viewHolder=(TodayAdapter_Meetroom.ViewHolder) view.getTag();
 
         }
+        /**
+         * 注意：我们在此给响应点击事件的区域（我的例子里是 showArea 的线性布局）添加Tag，为了记录点击的 position，我们正好用 position 设置 Tag
+         * 若监听整个item布局该处使用viewHolder.item1.setTag(position)
+         * writen by stu爸爸
+         */
+        viewHolder.img_order_show.setTag(position);
         //设置文字
-        viewHolder.tv_address.setText(datas.get(i));
+        viewHolder.tv_address.setText(datas.get(position));
         viewHolder.tv_peoplenum.setText("12-14人");
         viewHolder.tv_floor.setText("第4层");
         viewHolder.tv_price.setText("150元/30分钟");
         //日期和图片暂时不设置
 
+        //根据 currentItem 记录的点击位置来设置"对应Item"的可见性（在list依次加载列表数据时，每加载一个时都看一下是不是需改变可见性的那一条）
+        if (currentItem == position) {
+            viewHolder.item2.setVisibility(View.VISIBLE);
+        } else {
+            viewHolder.item2.setVisibility(View.GONE);
+        }
+        //可见的订购按钮点击事件、
+        viewHolder.img_order_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //用 currentItem 记录点击位置
+                int tag = (Integer) view.getTag();
+                if (tag == currentItem) { //再次点击
+                    currentItem = -1; //给 currentItem 一个无效值
+                } else {
+                    currentItem = tag;
+                }
+                Toast.makeText(context, "哈哈哈哈", Toast.LENGTH_SHORT).show();
+                //通知adapter数据改变需要重新加载
+                notifyDataSetChanged(); //必须有的一步
+            }
+        });
 
         return view;
     }
     private class ViewHolder{
         TextView tv_address,tv_peoplenum,tv_price,tv_floor;
-        ImageView img;
+        ImageView img,img_order_show;
+         RelativeLayout item1;
+        RelativeLayout item2;
         public ViewHolder(View view){
             //初始化TextView
             tv_floor=(TextView)view.findViewById(R.id.meetroom_floor_today);
@@ -71,7 +107,12 @@ public class TodayAdapter_Meetroom extends BaseAdapter {
             tv_price=(TextView)view.findViewById(R.id.meetroom_price_today);
             //初始化ImageView
             img=(ImageView)view.findViewById(R.id.meetroom_img_today);
-
+            //明面上的订购按钮
+            img_order_show=(ImageView)view.findViewById(R.id.meetroom_order_show);
+            //初始化item隐藏布局item2
+            item2=(RelativeLayout)view.findViewById(R.id.item2_meetroomtoday);
+            //初始化显示布局
+            item1=(RelativeLayout)view.findViewById(R.id.item1_meetroomtoday);
         }
     }
 }
--- app/src/main/java/com/ylg/workspace/workspace/fragment/TodayFragment_meetroom.java
+++ app/src/main/java/com/ylg/workspace/workspace/fragment/TodayFragment_meetroom.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.ylg.workspace.workspace.R;
 import com.ylg.workspace.workspace.adapter.TodayAdapter_Meetroom;
@@ -18,12 +21,13 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TodayFragment_meetroom extends Fragment {
+public class TodayFragment_meetroom extends Fragment implements View.OnClickListener{
     private List<String> datas;
     private ListView listView;
     private TodayAdapter_Meetroom todayAdapter;
-    private String[] address = new String[]{"东四多功能共享会议室"};
+    private String[] address = new String[]{"东四多功能共享会议室","人民大会堂","省办公厅第一会议室"};
     private View view;
+    private ImageView img_order;
 
     public TodayFragment_meetroom() {
         // Required empty public constructor
@@ -40,15 +44,33 @@ public class TodayFragment_meetroom extends Fragment {
     }
 
     private void init() {
+        //初始化img_order
+//        img_order=(ImageView)view.findViewById(R.id.meetroom_order_show);
+//        img_order.setOnClickListener(this);
         //初始化listview
         listView = (ListView)view.findViewById(R.id.meetroom_today_lv);
         //初始化集合并赋值(其他字段先不加值)
         datas = new ArrayList<>();
         datas.add(address[0]);
+        datas.add(address[1]);
+        datas.add(address[2]);
         //初始化adapter
         todayAdapter = new TodayAdapter_Meetroom(view.getContext(),datas);
         //绑定adapter
         listView.setAdapter(todayAdapter);
+        //listview的item点击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+            }
+        });
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.meetroom_order_show:
+                break;
+        }
+    }
 }
